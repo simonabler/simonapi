687209380aa5095be9da2e96346f25d33d4e0c0f
Simon Abler <simon.abler@ematric.com>
2025-09-05 20:57:16 +0200

    feat: apply throttler

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { QrModule } from './qr/qr.module';
 import { UtilityModule } from './utils/utility.module';
 import { ReportsController } from './reports.controller';
-import { seconds, ThrottlerModule } from '@nestjs/throttler';
+import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { WatermarkModule } from './watermark/watermark.module';
 import { BarcodesModule } from './barcode/barcodes.module';
 import { SignpackModule } from './signpack/signpack.module';
@@ -12,7 +12,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config';
 //dummyload for NX project.json
-import { } from 'sqlite3'
+import {} from 'sqlite3'
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -48,7 +49,7 @@ import { } from 'sqlite3'
     }),
     ThrottlerModule.forRoot({
       // v5+: mehrere „throttlers“ möglich; ttl in **Millisekunden**
-      throttlers: [{ ttl: seconds(60), limit: 60 }],
+      throttlers: [{ ttl: seconds(60), limit: 10 }],
       // Tracker = Schlüssel je Nutzer: API-Key bevorzugen, sonst IP
       getTracker: (req: any) => req.headers['x-api-key']?.toString().trim() || req.ip,
       // Optional: eigenen Redis-Storage o.ä. einsetzen (siehe unten)
@@ -61,6 +62,11 @@ import { } from 'sqlite3'
     UtilityModule,
   ],
   controllers: [AppController, ReportsController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
