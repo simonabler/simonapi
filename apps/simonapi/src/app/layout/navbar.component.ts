d3a0aa5255a9f57a8fffd797bf78dc9421193e27
Simon Abler <simon.abler@ematric.com>
2025-09-11 17:44:59 +0200

    feat(frontend): style

--- apps/simonapi/src/app/layout/navbar.component.ts
+++ apps/simonapi/src/app/layout/navbar.component.ts
@@ -2,16 +2,15 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { HammerIcon, LucideAngularModule } from 'lucide-angular';
 
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [LucideAngularModule],
   templateUrl: './navbar.component.html',
-
 })
 export class NavbarComponent {
   readonly HammerIcon = HammerIcon;
+  isOpen = false;
 
-
+  toggleNav() { this.isOpen = !this.isOpen; }
 }
